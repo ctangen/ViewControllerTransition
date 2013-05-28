ab33480736356181496d453e62d8da7cbb197aab
Tangen <ctangen@Cameron-Tangens-MacBook-Pro.local>
2013-05-28 13:31:20 -0700

    Initial Commit

---
+++ ViewControllerTransition/CSTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CSTAppDelegate.h
+//  ViewControllerTransition
+//
+//  Created by Tangen, Cameron on 5/28/13.
+//  Copyright (c) 2013 Tangen, Cameron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CSTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ViewControllerTransition/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ViewControllerTransition
+//
+//  Created by Tangen, Cameron on 5/28/13.
+//  Copyright (c) 2013 Tangen, Cameron. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CSTAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CSTAppDelegate class]));
+    }
+}
